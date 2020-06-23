793703660defc9bd90a79ccdbfaf5f17fa66f11c
zharkowski <maxim199919@rambler.ru>
2020-06-23 21:21:40 +0300

    рендерит начальную страницу

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import WelcomeScreen from "../welcome-screen/welcome-screen";
+import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 
 const App = () => {
   return (
