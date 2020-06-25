da3e5f4b91c3c752ac4d656b8a8d95f1adad76e0
zharkowski <maxim199919@rambler.ru>
2020-06-25 15:32:59 +0300

    фикс WelcomeScreen

--- src/components/welcome-screen/welcome-screen.jsx
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const WelcomeScreen = (props) => {
-  const errorsCount = props;
+  const {errorsCount} = props;
   return (
     <section className="main" id="root">
       <section className="welcome">
