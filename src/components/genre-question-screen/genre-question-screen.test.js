9cc3e9fa0127d1c8df930e9c70c1c6e3d3d360fc
zharkowski <maxim199919@rambler.ru>
2020-06-29 19:52:27 +0300

    добавляет снепшот тесты для новых компонентов и обновляет для App

---
+++ src/components/genre-question-screen/genre-question-screen.test.js
@@ -0,0 +1,33 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import GenreQuestionScreen from "./genre-question-screen";
+
+const question = {
+  mode: `genre`,
+  genre: `rock`,
+  answers: [{
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    genre: `rock`,
+  }, {
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    genre: `blues`,
+  }, {
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    genre: `jazz`,
+  }, {
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    genre: `rock`,
+  }],
+};
+
+describe(`Render GenreQuestionScreen`, () => {
+  it(`Should GenreQuestionScreen render correctly`, () => {
+    const tree = renderer.create(
+        <GenreQuestionScreen
+          question={question}
+        />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
