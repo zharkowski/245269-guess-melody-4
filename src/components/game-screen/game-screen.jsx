b380ee093ffd79202e5f78585cad5921453ccf98
zharkowski <maxim199919@rambler.ru>
2020-07-09 18:19:57 +0300

    добавлен компонент GameScreen

---
+++ src/components/game-screen/game-screen.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {GameMode} from "../../const";
+
+const GameScreen = (props) => {
+  const {mode, children} = props;
+
+  return (
+    <section className={`game game--${mode}`}>
+      <header className="game__header">
+        <a className="game__back" href="#">
+          <span className="visually-hidden">Сыграть ещё раз</span>
+          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию"/>
+        </a>
+
+        <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+          <circle className="timer__line" cx="390" cy="390" r="370"
+            style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}/>
+        </svg>
+
+        <div className="game__mistakes">
+          <div className="wrong"/>
+          <div className="wrong"/>
+          <div className="wrong"/>
+        </div>
+      </header>
+
+      {children}
+    </section>
+  );
+};
+
+GameScreen.propTypes = {
+  mode: PropTypes.oneOf([GameMode.GENRE, GameMode.ARTIST]).isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+};
