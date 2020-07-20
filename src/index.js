9d1b8f33dc87140f2018e9f866b78113cca3d24e
zharkowski <maxim199919@rambler.ru>
2020-07-21 00:24:13 +0300

    редьюсер добавлен в работу приложения

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 import settings from "./mocks/settings";
 import questions from "./mocks/questions";
+import {createStore} from "redux";
+import {reducer} from "./reducer";
+import {Provider} from "react-redux";
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-    <App
-      errorsCount={settings.ERRORS_COUNT}
-      questions={questions}
-    />,
+    <Provider store={store}>
+      <App
+        errorsCount={settings.ERRORS_COUNT}
+        questions={questions}
+      />
+    </Provider>,
     document.getElementById(`root`)
 );
