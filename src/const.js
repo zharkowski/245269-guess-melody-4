300e3626cc85804ca3c31a2a7f31d3ee93485b43
zharkowski <maxim199919@rambler.ru>
2020-07-21 00:17:34 +0300

    описан редьюсер

--- src/const.js
+++ src/const.js
@@ -2,3 +2,8 @@ export const GameMode = {
   GENRE: `genre`,
   ARTIST: `artist`,
 };
+
+export const ActionType = {
+  INCREMENT_MISTAKES: `INCREMENT_MISTAKES`,
+  INCREMENT_STEP: `INCREMENT_STEP`,
+};
---
+++ src/redux.js
@@ -0,0 +1,22 @@
+import {extend} from "./utils";
+import {ActionType} from "./const";
+
+const initialState = {
+  mistakes: 0,
+  step: -1,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.INCREMENT_MISTAKES:
+      return extend(state, {
+        mistakes: state.mistakes + action.payload,
+      });
+    case ActionType.INCREMENT_STEP:
+      return extend(state, {
+        step: state.step + action.payload,
+      });
+    default:
+      return state;
+  }
+};
