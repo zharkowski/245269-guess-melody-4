0d64266706396c50087efcb8265ec077c3229d02
zharkowski <maxim199919@rambler.ru>
2020-07-21 01:25:15 +0300

    фикс для работы redux dev tool

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import {createStore} from "redux";
 import {reducer} from "./reducer";
 import {Provider} from "react-redux";
 
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
 
 ReactDOM.render(
     <Provider store={store}>
