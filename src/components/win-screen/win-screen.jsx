8044252207c1ed03736839573942ad9bd8a1c54a
zharkowski <maxim199919@rambler.ru>
2020-08-02 18:31:04 +0300

    добавлены компоненты экранов проигрыша и победы и снепшот тесты для них

---
+++ src/components/win-screen/win-screen.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const WinScreen = (props) => {
+  const {onRetryButtonClick, questionsCount, mistakesCount} = props;
+  return (
+    <section className="result">
+      <div className="result__logo"><img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83" /></div>
+      <h2 className="result__title">Вы настоящий меломан!</h2>
+      <p className="result__total">Вы ответили правильно на {questionsCount} вопросов и совершили {mistakesCount} ошибки</p>
+      <button className="replay" type="button" onClick={onRetryButtonClick}>Сыграть ещё раз</button>
+    </section>
+  );
+};
+
+WinScreen.propTypes = {
+  onRetryButtonClick: PropTypes.func.isRequired,
+  questionsCount: PropTypes.number.isRequired,
+  mistakesCount: PropTypes.number.isRequired,
+};
+
+export default WinScreen;
