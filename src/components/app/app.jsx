2567633a556f57105dd712a1198b9ee70ccb541e
zharkowski <maxim199919@rambler.ru>
2020-08-02 15:02:10 +0300

    подключен HOC withAudio

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -6,7 +6,7 @@ import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 import GameScreen from "../game-screen/game-screen.jsx";
 import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen.jsx";
 import GenreQuestionScreen from "../genre-question-screen/genre-question-screen.jsx";
-import withActivePlayer from "../../hocs/with-audio-player/with-audio-player";
+import withActivePlayer from "../../hocs/with-active-player/with-active-player";
 import {GameMode} from "../../const";
 import {ActionCreator} from "../../reducer";
 
--- src/hocs/with-active-player/with-active-player.js
+++ src/hocs/with-active-player/with-active-player.js
@@ -1,5 +1,8 @@
 import React from "react";
-import AudioPlayer from "../../components/audio-player/audio-player.jsx";
+import Player from "../../components/audio-player/audio-player.jsx";
+import withAudio from "../with-audio/with-audio";
+
+const AudioPlayer = withAudio(Player);
 
 const withActivePlayer = (Component) => {
   class WithActivePlayer extends React.PureComponent {
